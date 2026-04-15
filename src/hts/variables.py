0ffa10a8e1a2ec43102482883c80f751705b4020
Ardalan <ardalan.mansouri@gmail.com>
2026-04-15 14:56:34 +0200

    add variable (name of selected compounds)

---
+++ src/hts/variables.py
@@ -0,0 +1,38 @@
+# Define the list of compounds that selected for validation 
+# from lopac and prestwick libraries. 
+LOPAC_INHIB_SELECTED = [
+    "AC-93253 iodide",
+    "(±)-p-Aminoglutethimide",
+    "O-Methylserotonin hydrochloride",
+    "HI-TOPK-032",
+    "Oxaprozin"
+]
+
+LOPAC_INDUC_SELECTED = [
+    "Dopamine hydrochloride",
+    "Aminoguanidine hydrochloride",
+    "U-69593",
+    "Piperlongumine",
+    "Methiothepin mesylate"
+]
+
+PRESTWICK_INHIB_SELECTED = [
+    "Verteporfin",
+    "Zaleplon",
+    "Troxipide",
+    "Digoxin",
+    "Hexetidine",
+    "Pindolol",
+    "Alprenolol hydrochloride",
+    "Monensin sodium salt",
+    "Fluvastatin sodium salt",
+    "Famprofazone"
+]
+
+PRESTWICK_INDUC_SELECTED = [
+    "Liranaftate",
+    "Amprolium hydrochloride",
+    "Flunisolide",
+    "Ciprofloxacin hydrochloride monohydrate",
+    "Dyclonine hydrochloride"
+]
